8d397a00851ed25b5483f4521ba412c1492b3dee
quzhi1 <quzhi65222714@gmail.com>
2016-03-26 15:58:48 -0700

    Mar update

--- BillCalculation.py
+++ BillCalculation.py
@@ -29,15 +29,21 @@ class Bill:
 
 # augBill = Bill("Aug", 180.5, [36.08, 10.06, 0.00, 7.96, 1.80])
 # septBill = Bill("Sept", 192.01, [36.08, 10.00, 0.20, 0.00, 0.00])
-octBill = Bill("Oct", 180.82, [36.08, 10.00, 0.00, 0.00, 0.00])
-novBill = Bill("Nov", 180.60, [36.08, 10.00, 0.00, 1.00, 0.00])
+#octBill = Bill("Oct", 180.82, [36.08, 10.00, 0.00, 0.00, 0.00])
+#novBill = Bill("Nov", 180.60, [36.08, 10.00, 0.00, 1.00, 0.00])
+decBill = Bill("Dec", 181.51, [36.08, 10.00, 0.00, 0.00, 0.00])
+janBill = Bill("Mar", 206.54, [36.08, 10.00, 0.00, 0.00, 0.00])
+febBill = Bill("Mar", 181.22, [36.08, 10.00, 0.00, 0.00, 0.00])
+marBill = Bill("Mar", 184.49, [36.08, 10.00, 0.00, 0.00, 0.00])
 
 # augBill.printSummary();
 # septBill.printSummary();
-octBill.printSummary();
-novBill.printSummary();
+#octBill.printSummary();
+#novBill.printSummary();
 
 for i in range(5):
-    octArray = octBill.getPersonalArray()
-    novArray = novBill.getPersonalArray()
-    print("%.2f" % (octArray[i] + novArray[i]))
+    decArray = decBill.getPersonalArray()
+    janArray = janBill.getPersonalArray()
+    febArray = febBill.getPersonalArray()
+    marArray = marBill.getPersonalArray()
+    print("%.2f" % (decArray[i] + janArray[i] + febArray[i] + marArray[i]))
